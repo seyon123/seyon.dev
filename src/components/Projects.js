399638583650b4e994c6ebf4a2e0ac809d43190d
Seyon Rajagopal <r_seyon@live.ca>
2021-12-27 13:16:25 -0500

    fetch projects from external source

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,32 @@
-import React from "react";
-import { ProjectItems } from "./ProjectItems";
+import React, { useEffect, useState } from "react";
 import Project from "./Project";
+import axios from "axios";
 
 import "./Projects.css";
 
 function Projects() {
+	const [projects, setProjects] = useState([]);
+
+	const fetchProjects = async () => {
+		let url = `https://seyon123.github.io/jsons/seyon-dev/projects.json`;
+		try {
+			const { data } = await axios.get(url);
+
+			setProjects(data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchProjects();
+	}, []);
+
 	return (
 		<section id="projects">
 			<h2 className="header">Projects</h2>
 
-			{ProjectItems.map(({ name, info, image, demoLink, githubLink, languages }) => (
+			{projects.map(({ name, info, image, demoLink, githubLink, languages }) => (
 				<Project key={name} name={name} info={info} image={image} demoLink={demoLink} githubLink={githubLink} languages={languages} />
 			))}
 		</section>
