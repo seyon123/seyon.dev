96d3501a50ff50b191b116248d26b08adca5e7c8
Seyon Rajagopal <r_seyon@live.ca>
2021-12-27 21:13:38 -0500

    update functions

--- functions/index.js
+++ functions/index.js
@@ -12,6 +12,7 @@ exports.preRender = functions.https.onRequest((request, response) => {
         
     // Getting the path
     const path = request.path ? request.path.split('/') : request.path;
+    console.log(path);
 
     // Getting index.html text
     let index = fs.readFileSync('./web/index.html').toString();
@@ -26,8 +27,9 @@ exports.preRender = functions.https.onRequest((request, response) => {
     }
     
     // Navigation menu
-    if     (path[1] === '')    setMetas('Portfolio - Seyon Rajagopal', 'Hello! My name is Seyon Rajagopal, and I am a Computer Science Co-op student at Ryerson University. I build, design and develop websites and applications.', 'https://seyon.dev/');
-    else if(path[1] === 'resume')    setMetas('Resume - Seyon Rajagopal', 'Hello! My name is Seyon Rajagopal. View My Resume!', 'https://seyon.dev/resume');
+    if(path[1] === 'resume')    setMetas('Resume - Seyon Rajagopal', 'Hello! My name is Seyon Rajagopal. View My Resume!', 'https://seyon.dev/resume');
+    else if(path[1] === '404')    setMetas('404 - Seyon Rajagopal', 'This page does not exist!', 'https://seyon.dev/404');
+    else setMetas('Portfolio - Seyon Rajagopal', 'Hello! My name is Seyon Rajagopal, and I am a Computer Science Co-op student at Ryerson University. I build, design and develop websites and applications.', 'https://seyon.dev/');
 
     
     // Sending index.html    
