cb8f6adebfb26b80661bc6bd2dffd5a779ca61fe
Seyon Rajagopal <r_seyon@live.ca>
2021-12-28 09:19:23 -0500

    remove functions

--- functions/index.js
+++ functions/index.js
@@ -7,23 +7,7 @@ admin.initializeApp();
 
 // Getting and replacing meta tags
 exports.preRender = functions.https.onRequest((request, response) => {
-	const userAgent = request.headers["user-agent"].toLowerCase();
-
-	const isBot =
-		userAgent.includes("googlebot") ||
-		userAgent.includes("yahoou") ||
-		userAgent.includes("bingbot") ||
-		userAgent.includes("baiduspider") ||
-		userAgent.includes("yandex") ||
-		userAgent.includes("yeti") ||
-		userAgent.includes("yodaobot") ||
-		userAgent.includes("gigabot") ||
-		userAgent.includes("ia_archiver") ||
-		userAgent.includes("facebookexternalhit") ||
-		userAgent.includes("twitterbot") ||
-		userAgent.includes("developers.google.com")
-			? true
-			: false;
+	
 	// Getting the path
 	const path = request.path ? request.path.split("/") : request.path;
 	console.log(path);
@@ -32,7 +16,7 @@ exports.preRender = functions.https.onRequest((request, response) => {
 	let index = fs.readFileSync("./index.html").toString();
 	var call = request.headers["x-forwarded-for"];
 	console.log(call);
-    
+
 	// Changing metas function
 	const setMetas = (title, description, url) => {
 		index = index
@@ -41,15 +25,15 @@ exports.preRender = functions.https.onRequest((request, response) => {
 			.replace(/__URL__/g, url);
 	};
 
-	setMetas(
-		"Portfolio - Seyon Rajagopal",
-		"Hello! My name is Seyon Rajagopal, and I am a Computer Science Co-op student at Ryerson University. I build, design and develop websites and applications.",
-		"https://seyon.dev/"
-	);
-
 	// Navigation menu
 	if (path[1] === "resume") setMetas("Resume - Seyon Rajagopal", "Hello! My name is Seyon Rajagopal. View My Resume!", "https://seyon.dev/resume");
 	else if (path[1] === "404") setMetas("404 - Seyon Rajagopal", "This page does not exist!", "https://seyon.dev/404");
-
+	else {
+		setMetas(
+			"Portfolio - Seyon Rajagopal",
+			"Hello! My name is Seyon Rajagopal, and I am a Computer Science Co-op student at Ryerson University. I build, design and develop websites and applications.",
+			"https://seyon.dev/"
+		);
+	}
 	response.status(200).send(index);
 });
