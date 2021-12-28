f1339509ee6659c0c87878236e80c25e04ad9738
Seyon Rajagopal <r_seyon@live.ca>
2021-12-28 01:01:09 -0500

    update functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+
 const admin = require("firebase-admin");
 const fs = require("fs");
 
@@ -6,37 +7,49 @@ admin.initializeApp();
 
 // Getting and replacing meta tags
 exports.preRender = functions.https.onRequest((request, response) => {
+	const userAgent = request.headers["user-agent"].toLowerCase();
+
+	const isBot =
+		userAgent.includes("googlebot") ||
+		userAgent.includes("yahoou") ||
+		userAgent.includes("bingbot") ||
+		userAgent.includes("baiduspider") ||
+		userAgent.includes("yandex") ||
+		userAgent.includes("yeti") ||
+		userAgent.includes("yodaobot") ||
+		userAgent.includes("gigabot") ||
+		userAgent.includes("ia_archiver") ||
+		userAgent.includes("facebookexternalhit") ||
+		userAgent.includes("twitterbot") ||
+		userAgent.includes("developers.google.com")
+			? true
+			: false;
 	// Getting the path
 	const path = request.path ? request.path.split("/") : request.path;
 	console.log(path);
 
-	try {
-		// Getting index.html text
-		let index = fs.readFileSync("./index.html").toString();
+	// Getting index.html text
+	let index = fs.readFileSync("./index.html").toString();
+	var call = request.headers["x-forwarded-for"];
+	console.log(call);
+    
+	// Changing metas function
+	const setMetas = (title, description, url) => {
+		index = index
+			.replace(/__TITLE__/g, title)
+			.replace(/__DESCRIPTION__/g, description)
+			.replace(/__URL__/g, url);
+	};
 
-		// Changing metas function
-		const setMetas = (title, description, url) => {
-			index = index
-				.replace(/__TITLE__/g, title)
-				.replace(/__DESCRIPTION__/g, description)
-				.replace(/__URL__/g, url);
-		};
+	setMetas(
+		"Portfolio - Seyon Rajagopal",
+		"Hello! My name is Seyon Rajagopal, and I am a Computer Science Co-op student at Ryerson University. I build, design and develop websites and applications.",
+		"https://seyon.dev/"
+	);
 
-		// Navigation menu
-		if (path[1] === "resume") setMetas("Resume - Seyon Rajagopal", "Hello! My name is Seyon Rajagopal. View My Resume!", "https://seyon.dev/resume");
-		else if (path[1] === "404") setMetas("404 - Seyon Rajagopal", "This page does not exist!", "https://seyon.dev/404");
-		else if (path[1] === "")
-			setMetas(
-				"Portfolio - Seyon Rajagopal",
-				"Hello! My name is Seyon Rajagopal, and I am a Computer Science Co-op student at Ryerson University. I build, design and develop websites and applications.",
-				"https://seyon.dev/"
-			);
+	// Navigation menu
+	if (path[1] === "resume") setMetas("Resume - Seyon Rajagopal", "Hello! My name is Seyon Rajagopal. View My Resume!", "https://seyon.dev/resume");
+	else if (path[1] === "404") setMetas("404 - Seyon Rajagopal", "This page does not exist!", "https://seyon.dev/404");
 
-		// Sending index.html
-        response.set('Cache-Control', 'no-cache');
-		response.status(200).send(index);
-	} catch (error) {
-		console.log(error);
-		response.sendStatus(500);
-	}
+	response.status(200).send(index);
 });
