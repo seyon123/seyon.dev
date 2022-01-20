b483f759c71e99ca32467c4cdd559ca0120ced6d
Seyon Rajagopal <r_seyon@live.ca>
2022-01-19 21:33:39 -0500

    Add project page content

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,20 +1,51 @@
 import { useRouter } from "next/router";
+import Image from "next/image";
+import MetaTags from "../../components/MetaTags";
 import axios from "axios";
-import styles from "../../styles/Home.module.css";
+import styles from "../../styles/ProjectItem.module.css";
 
-export default function ProjectDetails({projectItem}) {
-    const router = useRouter();
 
-    console.log(projectItem);
+export default function ProjectDetails({ projectItem }) {
+	const router = useRouter();
+
+	console.log(projectItem);
 
 	return (
 		<div className={styles.content}>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <h1> {projectItem.name} </h1>
+			<MetaTags title={`${projectItem.name} - Seyon Rajagopal`} description={projectItem.infoAlt} image={`/assets/projects/${projectItem.image}`} />
+			<h1> {projectItem.name} </h1>
+			<div className={styles["project-cover"]}>
+				<div className={styles.project}>
+					<div className={styles["info-bg"]}>
+						<div className={styles["project-info"]}>
+
+							<p dangerouslySetInnerHTML={{ __html: projectItem.info }}></p>
+							<div className={styles["project-links"]}>
+								<div className={styles.btns}>
+									{projectItem.demoLink ? (
+										<a href={projectItem.demoLink} className={styles.demo} target="_blank" aria-label="View Live Demo" rel="noopener noreferrer">
+											Live Demo
+										</a>
+									) : (
+										""
+									)}
+									<a href={projectItem.githubLink} target="_blank" className={styles.github} aria-label="View Source Code" rel="noopener noreferrer">
+										<i className="fab fa-github"></i>
+									</a>
+								</div>
+								<div className={styles.languages}>
+									{projectItem.languages.map((language, i) => (
+										<i key={i} title={projectItem.languagesAlt[i]} className={`fab ${language}`}></i>
+									))}
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className={styles["img-container"]}>
+						<Image className={styles.img} placeholder="blur" blurDataURL={`/assets/projects/${projectItem.image}`} width="1920" height="1080" alt={projectItem.name} src={`/assets/projects/${projectItem.image}`} />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
@@ -25,7 +56,7 @@ export async function getStaticProps({ params }) {
 
 	try {
 		const { data } = await axios.get(url);
-		projectItem = data.find(({webName}) => webName === params.slug);
+		projectItem = data.find(({ webName }) => webName === params.slug);
 	} catch (error) {
 		console.log(error);
 	}
@@ -39,16 +70,15 @@ export async function getStaticProps({ params }) {
 
 export async function getStaticPaths() {
 	let url = `https://seyon123.github.io/jsons/seyon-dev/projects-all.json`;
-	let paths = []
+	let paths = [];
 
-    const { data } = await axios.get(url);
-    paths = data.map(project => { return { params: { slug: project.webName } } });
+	const { data } = await axios.get(url);
+	paths = data.map((project) => {
+		return { params: { slug: project.webName } };
+	});
 
 	return {
 		paths,
-        fallback: false
-
+		fallback: false,
 	};
 }
-
-
